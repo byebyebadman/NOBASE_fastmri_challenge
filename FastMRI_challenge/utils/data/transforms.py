4d9132612bbb8ce08c3f038252d8c825092774f0
byebyebadman <cih0212@snu.ac.kr>
2023-08-07 18:50:14 +0000

    another commit

--- FastMRI_challenge/utils/data/transforms.py
+++ FastMRI_challenge/utils/data/transforms.py
@@ -38,21 +38,21 @@ class DataTransform:
             else:
                 kspace = to_tensor(input * mask)
                 kspace = torch.stack((kspace.real, kspace.imag), dim=-1)
-                mask = torch.from_numpy(mask.reshape(1, 1, input.shape[-2], 1).astype(np.float32)).byte()
+                mask = torch.from_numpy(mask.reshape(1, 1, kspace.shape[-2], 1).astype(np.float32)).byte()
                 target = to_tensor(target)
             maximum = attrs[self.max_key]
             
         elif not self.isforward and not self.use_augment:
             kspace = to_tensor(input * mask)
             kspace = torch.stack((kspace.real, kspace.imag), dim=-1)
-            mask = torch.from_numpy(mask.reshape(1, 1, input.shape[-2], 1).astype(np.float32)).byte()
+            mask = torch.from_numpy(mask.reshape(1, 1, kspace.shape[-2], 1).astype(np.float32)).byte()
             target = to_tensor(target)
             maximum = attrs[self.max_key]
             
         else:
             kspace = to_tensor(input * mask)
             kspace = torch.stack((kspace.real, kspace.imag), dim=-1)
-            mask = torch.from_numpy(mask.reshape(1, 1, input.shape[-2], 1).astype(np.float32)).byte()
+            mask = torch.from_numpy(mask.reshape(1, 1, kspace.shape[-2], 1).astype(np.float32)).byte()
             target = -1
             maximum = -1
                
